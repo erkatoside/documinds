e76520da1297201690b6095bf9f222ed208e8e29
erkatoside <anwarsystemanano@gmail.com>
2023-07-14 23:31:15 +0700

    styling my doc

--- app/docs/page.jsx
+++ app/docs/page.jsx
@@ -235,8 +235,19 @@ export default function Home() {
 
                 <div className="grid grid-cols-4 gap-6">
                   {docs.map((i) => (
-                    <Link key={i.id} href={`/chat/${i.ipfsHash}`} className="w-full bg-white p-4 shadow rounded-md">
-                      {i.name}
+                    <Link key={i.id} href={`/chat/${i.ipfsHash}`} className="w-full bg-white shadow rounded-md group hover:shadow-xl transition-all">
+                      <div className="w-full aspect-[3/1] bg-neutral-800 flex items-center justify-center rounded-t-md">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 text-white group-hover:text-lime-400">
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="p-3">
+                        <h6 className="line-clamp-1 text-sm font-medium">{i.name}</h6>
+                      </div>
                     </Link>
                   ))}
                 </div>
